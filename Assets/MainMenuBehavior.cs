01826f320335640687ae9528110bfc9ec121032f
bkalb <bkalb@blanketfortgames.com>
2018-01-26 22:30:53 -0500

    Started a menu Started a player class

---
+++ Assets/MainMenuBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets;
+using InControl;
+using UnityEngine;
+
+public class MainMenuBehavior : MonoBehaviour
+{
+    private Player[] players;
+    private float _countdownTime;
+
+	// Use this for initialization
+	void Start () {
+		players = new Player[4];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    for (int i = 0; i < InputManager.Devices.Count; i++)
+	    {
+	        if (InputManager.Devices[i].IsAttached && InputManager.Devices[i].AnyButtonWasPressed)
+	        {
+	            for (int j = 0; j < players.Length; j++)
+	            {
+	                if (players[i] == null)
+	                {
+                        players[i] = new Player(InputManager.Devices[i]);
+	                    break;
+	                }
+	                if (players[i].Device == InputManager.Devices[i])
+	                {
+	                    break;
+	                }
+
+	            }
+	        }
+	    }
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InControl;
+
+namespace Assets
+{
+    class Player
+    {
+        public Player(InputDevice inputDevice)
+        {
+            Device = inputDevice;
+			Ready = true;
+        }
+
+        public InputDevice Device { get; set; }
+
+		public bool Ready { get; set; }
+		
+		//TODO:  Add current book, add hitpoint books, add head size, add powerups, etc.
+    }
+}
