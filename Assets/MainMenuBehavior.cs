8f3535dfd56c5e473d2713c2948c9ec62ec86b09
bkalb <bkalb@blanketfortgames.com>
2018-01-27 08:39:37 -0500

    Main Menu functionality complete still needs instructions

--- Assets/MainMenuBehavior.cs
+++ Assets/MainMenuBehavior.cs
@@ -1,17 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Assets;
+using DG.Tweening;
 using InControl;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuBehavior : MonoBehaviour
 {
+    public Button[] Buttons;
+    public Text TimerText;
+
     private Player[] players;
     private float _countdownTime;
 
 	// Use this for initialization
 	void Start () {
 		players = new Player[4];
+	    _countdownTime = -100;
+	    DOTween.Init();
 	}
 	
 	// Update is called once per frame
@@ -25,6 +34,11 @@ public class MainMenuBehavior : MonoBehaviour
 	                if (players[i] == null)
 	                {
                         players[i] = new Player(InputManager.Devices[i]);
+	                    Buttons[i].transform.DOMoveY(Buttons[i].transform.position.y + 10, 1).SetEase(Ease.OutElastic);
+	                    if (_countdownTime < -90 && players.Count(p=>p != null && p.Device.IsAttached) > 1)
+	                    {
+	                        _countdownTime = 10;
+	                    }
 	                    break;
 	                }
 	                if (players[i].Device == InputManager.Devices[i])
@@ -34,6 +48,23 @@ public class MainMenuBehavior : MonoBehaviour
 
 	            }
 	        }
+	    }
+	    for (int i = 0; i < players.Length; i++)
+	    {
+	        if (players[i] != null)
+	        {
+	            Buttons[i].interactable = players[i].Device.IsAttached;
+	        }
+	    }
+	    if (_countdownTime > 0)
+	    {
+	        _countdownTime -= Time.deltaTime;
+            TimerText.text = ((int)_countdownTime + 1).ToString();
+	    }
+	    else if(_countdownTime > -90)
+	    {
+	        TimerText.text = "Starting...";
+            SceneManager.LoadScene("EmptyMap");
 	    }
 	}
 }
