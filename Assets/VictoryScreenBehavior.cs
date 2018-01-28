cec2170de09a55fec494862450d44e9628cf1bcd
MLaurentys <matheus_t.laurentys@hotmail.com>
2018-01-28 12:03:55 -0500

    start of a more responsive game ending screen

--- Assets/VictoryScreenBehavior.cs
+++ Assets/VictoryScreenBehavior.cs
@@ -15,7 +15,7 @@ public class VictoryScreenBehavior : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-	    _countdownTimer = 20;
+	    _countdownTimer = 22;
 	    quoteCategory = PlayerHolderBehavior.singleton.LastHit;
 		played = false;
         //TODO: Load up a quote
@@ -27,10 +27,13 @@ public class VictoryScreenBehavior : MonoBehaviour
 	{
         //TODO: Wait until the quote is done
 	    _countdownTimer -= Time.deltaTime;
-		if (!played && _countdownTimer < 15) {
+		if (!played && _countdownTimer < 20) {
 			int track = Random.Range (0, 6);
-			audioSource.PlayOneShot (clips [(int)quoteCategory * 5 + track]);
+			audioSource.PlayOneShot (clips [1 * 5 + track]);
 			played = true;
+		}
+		if (_countdownTimer < 9) {
+			
 		}
 	    if (_countdownTimer < 0)
 	    {
