e1c4dbb2ef95cad50aea2b91618482a99f5e6144
bkalb <bkalb@blanketfortgames.com>
2018-01-28 10:47:52 -0500

    Ghost Particle

--- Assets/BaseGameplay/RecallAbility.cs
+++ Assets/BaseGameplay/RecallAbility.cs
@@ -10,6 +10,9 @@ public class RecallAbility : CooldownAbility {
     public LayerMask areaCheckLayerMask;
     public GameObject ghostPrefab;
 
+    public GameObject ParticlePrefab;
+    protected ParticleSystem particles;
+
     protected Queue<Vector3> oldPositions = new Queue<Vector3>();
     protected RigidbodyController controller;
     protected Player player;
@@ -20,6 +23,11 @@ public class RecallAbility : CooldownAbility {
         controller = GetComponent<RigidbodyController>();
         player = controller.player;
         ghostObject = Instantiate(ghostPrefab, transform.position, transform.rotation);
+
+        particles = Instantiate(ParticlePrefab, transform)
+            .GetComponent<ParticleSystem>();
+        particles.GetComponent<Renderer>().material.mainTexture =
+            controller.playerSprites[(int) controller.player.PlayerColor].texture;
     }
 
     private void Update()
@@ -28,32 +36,42 @@ public class RecallAbility : CooldownAbility {
         //recall if button is pressed
         if (player.Device.Action3.WasPressed && TryToUseAbility())
         {
-            var positions = oldPositions.ToArray();
-            bool abilityUsed = false;
-            for(int i = 0; i < positions.Length; i++)
-            {
-                var position = positions[i];
-                Vector2 positionToCheck = new Vector2(position.x, position.y);
-                var raycastHit = Physics2D.OverlapCircle(positionToCheck, areaCheckRadius, areaCheckLayerMask);
-                if(raycastHit == null)
-                {
-                    transform.position = position;
-                    oldPositions = new Queue<Vector3>();
-                    abilityUsed = true;
-                    ghostObject.SetActive(false);
-                    break;
-                }
-            }
-            if(!abilityUsed)
-            {
-                abilityAvalibleToUse = true;
-            }
+            StartCoroutine(GhostRecall());
         }
         if (oldPositions.Count > (tracebackTime / Time.fixedDeltaTime) / 2 && abilityAvalibleToUse)
         {
             ghostObject.transform.position = oldPositions.Peek();
             ghostObject.SetActive(true);
         }
+    }
+
+    private IEnumerator GhostRecall()
+    {
+        var positions = oldPositions.ToArray();
+        bool abilityUsed = false;
+        particles.Play();
+        yield return null;
+
+        for (int i = 0; i < positions.Length; i++)
+        {
+            var position = positions[i];
+            Vector2 positionToCheck = new Vector2(position.x, position.y);
+            var raycastHit = Physics2D.OverlapCircle(positionToCheck, areaCheckRadius, areaCheckLayerMask);
+            if (raycastHit == null)
+            {
+                transform.position = position;
+                oldPositions = new Queue<Vector3>();
+                abilityUsed = true;
+                ghostObject.SetActive(false);
+                break;
+            }
+        }
+        if (!abilityUsed)
+        {
+            abilityAvalibleToUse = true;
+        }
+        yield return null;
+        particles.Stop();
     }
 
     // Update is called once per frame
